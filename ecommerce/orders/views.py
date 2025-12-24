60a08fa446c31c37ca0d03732b55baca946229e1
vt-nanni26 <vt.nanni26@gmail.com>
2025-12-24 21:41:52 +0530

    start1

--- ecommerce/orders/views.py
+++ ecommerce/orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Order
+from products.models import Product
 
-# Create your views here.
+def place_order(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    Order.objects.create(
+        user=request.user,
+        product=product,
+        quantity=1,
+        total_price=product.price
+    )
+
+    return redirect('/products/')
