15e2e9ac260f5d0ad9a9a585a9051d5cf279fb63
vt-nanni26 <vt.nanni26@gmail.com>
2025-12-28 22:18:30 +0530

    backend  done

--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     if request.method == "POST":
@@ -25,3 +26,18 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/signup.html', {'form': form})
+
